d2f31c353169661a6de8414cff83bc7401776bbe
“seeinplays” <seeinplays@gmail.com>
2022-07-31 14:08:23 -0600

    floats and strings

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import math
+from tkinter.tix import INTEGER
 
-
+#3 INPUT
 # name = input("What is your name? ")
 # print("Hello " + name)
 
-birth_year = input("Type your year of birth? ")
-age = 2022 - int(birth_year)
+## INTEGER
+# birth_year = input("Type your year of birth? ")
+# age = 2022 - int(birth_year)
 
-print("Your age is " + age)
+# print("Your age is " + age)
+
+# FLOAT
+first = input("first ")
+second = input("second ")
+
+sum = float(first) + float(second)
+
+print("Sum is " + str(sum))
