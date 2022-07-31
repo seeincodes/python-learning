6a63cd64f5f23fa9c89c7453393a628a78cd0c5b
“seeinplays” <seeinplays@gmail.com>
2022-07-30 18:37:47 -0600

    string to int math

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-name = input("What is your name? ")
-print("Hello " + name)
+import math
+
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+birth_year = input("Type your year of birth? ")
+age = 2022 - int(birth_year)
+
+print("Your age is " + age)
